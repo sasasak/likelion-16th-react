2efaa2764e5e9b7dd53bc3e244326a5bc34f973b
sasasak <geun02young@naver.com>
2026-03-06 17:11:27 +0900

    config. Vite, TS 구성(Config) - Vite: resolve.alias - TS: baseUrl, paths

--- learning-react/src/app/App.tsx
+++ learning-react/src/app/App.tsx
@@ -1,4 +1,5 @@
-import JsxExpression from '../components/jsx-expression/JsxExpression'
+// ../ 대신 @로 대체 가능해짐!
+import JsxExpression from '@/components/jsx-expression/JsxExpression'
 
 export default function App() {
   return (
