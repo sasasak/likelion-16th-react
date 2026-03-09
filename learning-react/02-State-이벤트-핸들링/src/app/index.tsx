34b93aca2ab8944bed3d7d5a0590cfd97afc65f9
sasasak <geun02young@naver.com>
2026-03-09 10:34:29 +0900

    learn. 컴포넌트 속성(Props) - Props는 객체 타입 - TypeScript 인라인 타입 정의

--- learning-react/02-State-이벤트-핸들링/src/app/index.tsx
+++ learning-react/02-State-이벤트-핸들링/src/app/index.tsx
@@ -1,11 +1,19 @@
+// import { createElement } from 'react'
 import Footer from '@/components/Footer'
 import S from './style.module.css'
 
 export default function App() {
   return (
     <div className={S.container}>
-      <h1>React App</h1>
-      <Footer />
+      <div data-placeholder />
+      <Footer slogan={2026 + ''} />
+
+      {/* {createElement(
+        // 컴포넌트 이름 (Component Name)
+        Footer,
+        // 컴포넌트 속성 (Component Props)
+        { slogan: '완주 이후엔 스스로 학습 가능!' },
+      )} */}
     </div>
   )
 }
--- learning-react/02-State-이벤트-핸들링/src/components/Footer/index.tsx
+++ learning-react/02-State-이벤트-핸들링/src/components/Footer/index.tsx
@@ -1,6 +1,19 @@
 import S from './style.module.css'
 
-export default function Footer() {
+export default function Footer(props: { slogan: string }) {
+  /**
+   * Props로 데이터 전달(부모 → 자식)
+   * - ✅ 부모에서 자식으로 전달하는 데이터
+   * - ✅ React.createElement API에서 Props 객체
+   * - [] Props 타입 정의 (인라인 → 인터페이스)
+   */
+  // const slogan = '완벽보다 완주를!'
+
+  // 부모(상위) 컴포넌트가 자식(하위) 컴포넌트에 전달한 데이터 집합 = Props (Properties)
+  // JavaScript : 함수이름(데이터) -> function 함수이름(매개변수) {}
+  // React : React.createElement(컴포넌트이름, { 키: 값 }) -> function 컴포넌트이름(props) {}
+  // console.log({ props }, props.slogan)
+
   /**
    * 데이터 바인딩 (Data Binding = Data + JSX = Markup)
    * - JavaScript 데이터(변수, 상수, 계산된 값 등)
@@ -9,24 +22,16 @@ export default function Footer() {
   const currentYear = new Date().getFullYear() // 숫자 값이 아닌, 날짜 객체 현재 년도 가져오기
 
   /**
-   * Props로 데이터 전달(부모 → 자식)
-   * - 부모에서 자식으로 전달하는 데이터
-   * - React.createElement API에서 Props
-   * - Props 타입 정의 (인라인 → 인터페이스)
-   */
-  const slogan = '완벽보다 완주를!'
-
-  /**
    * 접근성 고려
    * - 사용자가 이해하기 쉽고 읽기 쉽도록 저작권 레이블(label) 구성
    */
-  const copyrightLabel = `모든 저작권은 이듬(EUID)에 있습니다. ${slogan}`
+  const copyrightLabel = `모든 저작권은 이듬(EUID)에 있습니다. ${props.slogan}`
 
   return (
     <footer className={S.footer}>
       <small aria-label={copyrightLabel}>
         {currentYear} COPYRIGHT RESERVED. © <abbr title="이듬(EUID)">EUID</abbr>
-        . {slogan}
+        . {props.slogan}
       </small>
     </footer>
   )
