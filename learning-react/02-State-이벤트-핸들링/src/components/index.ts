3089d8cdb2b7a38ff1228c80bb9dd87c8e89a3af
sasasak <geun02young@naver.com>
2026-03-09 14:34:56 +0900

    learn. 다시 내보내기(re-export) 구문

--- learning-react/02-State-이벤트-핸들링/src/components/index.ts
+++ learning-react/02-State-이벤트-핸들링/src/components/index.ts
@@ -6,4 +6,10 @@
 // 3. components/Wrapper ✅
 // 4. components/Section ✅
 // 5. components/Button ✅
-// 6. 다시 내보내기(re-export) 구문
+// 6. 다시 내보내기(re-export) 구문 ✅
+
+export { default as Button } from './Button'
+export { default as Footer } from './Footer'
+export { default as Header } from './Header'
+export { default as Image } from './Image'
+export { default as Section } from './Section'
