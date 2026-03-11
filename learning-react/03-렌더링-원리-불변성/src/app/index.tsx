3446bf9d4d6921ed9ffbaa91fda346a5a7d1ea4d
sasasak <geun02young@naver.com>
2026-03-11 10:09:47 +0900

    learn. 가상 DOM

--- learning-react/03-렌더링-원리-불변성/src/app/index.tsx
+++ learning-react/03-렌더링-원리-불변성/src/app/index.tsx
@@ -1,10 +1,10 @@
-import { RenderPipeline } from '@/learns'
+import { VirtualDOM } from '@/learns'
 import S from './style.module.css'
 
 export default function App() {
   return (
     <div className={S.container}>
-      <RenderPipeline />
+      <VirtualDOM />
     </div>
   )
 }
