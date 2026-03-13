3ab89f75fe7cf0abf0213dc002be1186b1392ec3
sasasak <geun02young@naver.com>
2026-03-13 15:31:33 +0900

    learn. 이펙트(Effect) 기초 - 순수 함수 (리액트는 컴포넌트를 순수하게 유지하길 요구) - StrictMode의 역할 (개발 모드에서만 두 번 컴포넌트 실행해 순수한지 여부를 확인하여 문제가 발생할 수 있는 가능성을 개발자에게 안내) - 이펙트(설정함수): 렌더링 단계에서 벗어난 외부 시스템과 동기화 로직

--- learning-react/05-이펙트-비동기-통신/src/app/index.tsx
+++ learning-react/05-이펙트-비동기-통신/src/app/index.tsx
@@ -1,10 +1,10 @@
-import { RaceCondition } from '@/components'
+import { EffectBasic } from '@/components'
 import S from './style.module.css'
 
 export default function App() {
   return (
     <div className={S.container}>
-      <RaceCondition />
+      <EffectBasic />  
     </div>
   )
 }
