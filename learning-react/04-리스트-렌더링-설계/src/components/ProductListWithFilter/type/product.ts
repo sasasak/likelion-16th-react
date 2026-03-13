6bb87118d7aa8f5c970ed2e03d2ce77715fccf88
sasasak <geun02young@naver.com>
2026-03-13 10:26:39 +0900

    learn. Props 전달 (부모 → 자식) - 리액트는 데이터 흐름이 위에서 아래로 흐른다 - 단방향 데이터 흐름 (One-way Data Flow)

--- learning-react/04-리스트-렌더링-설계/src/components/ProductListWithFilter/type/product.ts
+++ learning-react/04-리스트-렌더링-설계/src/components/ProductListWithFilter/type/product.ts
@@ -1,6 +1,13 @@
+import type { Dispatch, SetStateAction } from "react"
+
 export interface Product {
   id: number
   name: string
   price: number
   isSoldOut: boolean
+}
+
+export interface ProductSearchProps {
+  searchQuery: string
+  setSearchQuery: Dispatch<SetStateAction<string>>
 }
