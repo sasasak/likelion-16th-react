ebfcdcee2f0c6edc01a51eb3549b2bdc9497e261
sasasak <geun02young@naver.com>
2026-03-18 17:37:42 +0900

    learn. Ref vs State - Ref 렌더링 ❌ - State 렌더링 ⭕️

--- learning-react/06-폼-핸들링-DOM-참조/src/components/RefStudy/parts/CounterComparison.tsx
+++ learning-react/06-폼-핸들링-DOM-참조/src/components/RefStudy/parts/CounterComparison.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/refs */
+import { useRef, useState } from 'react'
 import S from '../RefStudy.module.css'
 
 // --------------------------------------------------------
@@ -10,25 +12,40 @@ import S from '../RefStudy.module.css'
 // --------------------------------------------------------
 
 export default function CounterComparison() {
+  const [countState, setCountState] = useState(0)
+  const handleIncreamentState = () => setCountState((c) => c + 1)
   
+  const countRef = useRef(0) // RefObject { current: 0 }
+  const handleIncreamentRef = () => {
+    console.log(`Ref의 현재 값 ${countRef.current}`)
+    countRef.current += 1
+  }
+
   return (
     <section className={S.section}>
       <h3 className={S.title}>State vs Ref 비교</h3>
       <div className={S.display}>
         <div>
           State ( 렌더링 <span aria-label="처리">⭕️</span> ){' '}
-          <strong>{'상태 값'}</strong>
+          <strong>{countState}</strong>
         </div>
         <div>
           Ref ( 렌더링 <span aria-label="안함">❌</span> ){' '}
-          <strong>{'참조 값'}</strong>
+          <strong>{countRef.current}</strong>
         </div>
       </div>
       <div className={S.inputGroup}>
-        <button type="button" className={S.button}>
+        <button
+          type="button"
+          className={S.button}
+          onClick={handleIncreamentState}
+          >
           State 증가
         </button>
-        <button type="button" className={S.button}>
+        <button type="button" 
+          className={S.button}
+          onClick={handleIncreamentRef}
+        >
           Ref 증가
         </button>
       </div>
